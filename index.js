a2b695e95f147dabd751352f99b33bb9ae65b0e4
Hari Muralikrishnan <mhari2000@gmail.com>
2020-01-15 16:06:08 -0500

    basic functionality

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 function multiplyBy(){
 	// Insert your code here
+	num1 = document.getElementById("firstNumber").value;
+    num2 = document.getElementById("secondNumber").value;
+    document.getElementById("result").innerHTML = num1 * num2;
 }
 
 function divideBy(){ 
 	// Insert your code here
+	num1 = document.getElementById("firstNumber").value;
+    num2 = document.getElementById("secondNumber").value;
+	document.getElementById("result").innerHTML = num1 / num2;
 }
